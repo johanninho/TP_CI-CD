f595c77691c85a9b2b5d6e5c8b519d398b40299f
johann <johann.andriandafimanana@ynov.com>
2025-06-23 13:40:54 +0200

    add soustraction

--- test_simple_math.py
+++ test_simple_math.py
@@ -4,6 +4,8 @@ from simple_math import SimpleMath
 class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(SimpleMath.addition(2, 3), 5)
+    def test_soustraction(self):
+        self.assertEqual(SimpleMath.soustraction(5, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
